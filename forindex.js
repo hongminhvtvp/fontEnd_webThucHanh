03907527a11eb262851200bc5d545846c30226bb
hongminhvtvp <hongminhvtvp@gmail.com>
2021-02-20 23:17:46 +0700

    1 ngay dau tao thanh header chua xong

---
+++ forindex.js
@@ -0,0 +1,29 @@
+ 
+// var i=0;
+// const thoigian=3000;
+// var listanh=[];
+
+// listanh[0]="./images/anh1.jpg";listanh[1]="./images/anh2.jpg";listanh[2]="./images/anh3.jpg";listanh[3]="./images/anh4.jpg";
+// listanh[4]="./images/anh5.jpg";listanh[5]="./images/anh6.jpg";listanh[6]="./images/anh7.jpg";listanh[7]="./images/anh8.jpg";
+// listanh[8]="./images/anh9.jpg";listanh[9]="./images/anh10.jpg";listanh[10]="./images/anh11.jpg";listanh[11]="./images/anh12.jpg";
+// listanh[12]="./images/anh13.jpg";listanh[13]="./images/anh14.jpg";listanh[14]="./images/anh15.jpg";listanh[15]="./images/anh16.jpg";
+// listanh[16]="./images/anh17.jpg";listanh[17]="./images/anh16.jpg";listanh[18]="./images/anh19.jpg";listanh[19]="./images/anh20.jpg";
+// listanh[20]="./images/anh21.jpg";listanh[21]="./images/anh22.jpg";listanh[22]="./images/anh23.jpg";listanh[23]="./images/anh24.jpg";
+// listanh[24]="./images/anh25.jpg";listanh[25]="./images/anh26.jpg";listanh[26]="./images/anh27.jpg";listanh[27]="./images/anh28.jpg";
+// listanh[28]="./images/anh29.jpg";listanh[29]="./images/anh29.jpg";listanh[30]="./images/anh31.jpg";listanh[31]="./images/anh32.jpg";
+// listanh[32]="./images/anh33.jpg";listanh[33]="./images/anh34.jpg";listanh[34]="./images/anh35.jpg";listanh[35]="./images/anh36.jpg";
+// listanh[36]="./images/anh37.jpg";
+// function thaydoianh() {
+// 	let anhid=document.getElementById('anhid').src=listanh[i];
+// 	if (i < listanh.length - 1) {
+// 		i++;
+// 	}else{
+// 		i=0;
+// 	}
+// 	console.log("i thứ ",i);
+// 	return setTimeout(thaydoianh,thoigian);
+// 	//setTimeout("thaydoianh",thoigian);
+
+// }
+// window.onload=thaydoianh;
+// thaydoianh();
